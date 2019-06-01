136ceac6672e342f8e076d41779ab37e25d0f18f
Kingman Douglass <kreedd88@gmail.com>
2019-05-31 21:25:11 -0500

    got basic layout with no functionality set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,84 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    value: "",
+    firstNumber: "",
+    secondNumber: "",
+    symbol: "",
+  }
+
+  componentDidUpdate = () => {
+    console.log('state', this.state);
+    
+  }
+
+  empty = () => {
+  
+  }
+
+  handleChange = event => {
+    
+  }
+  
+  handleDecimalChange = event => {
+    
+  }
+
+  handleSymbolChange = event => {
+  
+  }
+
+  handleZeroChange = event => {
+   
+  }
+
+  submitEquation = () => {
+   
+  }
+
+
+render() {
   return (
     <div className="App">
-<p>TEST</p>
-    </div>
+    <div>
+      <div>
+        <div>{this.state.input}</div>
+      </div>
+     <div>
+      <button onClick={this.handleChange('input')} value="2">7</button>
+      <button onClick={this.handleChange('input')} value="2">8</button>
+      <button onClick={this.handleChange('input')} value="2">9</button>
+      <button onClick={this.handleSymbolChange('symbol')} value="/">/</button>
+     </div>
+     <div>
+      <button onClick={this.handleChange('input')} value="4">4</button>
+      <button onClick={this.handleChange('input')} value="5">5</button>
+      <button onClick={this.handleChange('input')} value="6">6</button>
+      <button onClick={this.handleSymbolChange('symbol')} value="*">*</button>
+     </div>
+     <div>
+      <button onClick={this.handleChange('input')} value="1">1</button>
+      <button onClick={this.handleChange('input')} value="2">2</button>
+      <button onClick={this.handleChange('input')} value="3">3</button>
+      <button onClick={this.handleSymbolChange('symbol')} value="+">+</button>
+     </div>
+     <div>
+      <button nClick={this.handleDecimalChange('input')} value="3">.</button>
+      <button onClick={this.handleZeroChange('input')} value="0">0</button>
+      <button onClick={this.submitEquation}>=</button>
+      <button onClick={this.handleSymbolChange('symbol')} value="-">-</button>
+     </div>
+     <div>
+      <button onClick={this.empty}>Clear</button>
+     </div>
+   </div>
+  </div>
   );
+  }
 }
 
 export default App;
