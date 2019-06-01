fb839129610f7704f5b118ecc351f860ef8ba424
Kingman Douglass <kreedd88@gmail.com>
2019-05-31 22:00:15 -0500

    functioning but the answer isnt working and dispatch wont be ready on submit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 import './App.css';
 
 class App extends Component {
@@ -17,27 +16,65 @@ class App extends Component {
   }
 
   empty = () => {
-  
+    this.setState({ value: "" })
+    this.setState({ firstNumber: "" })
+    this.setState({ secondNumber: "" })
+    this.setState({ symbol: "" })
   }
 
-  handleChange = event => {
-    
+  handleChange = number => event => {
+    this.setState({
+      value: this.state.value + event.target.value,
+  })
   }
   
-  handleDecimalChange = event => {
-    
+  handleDecimalChange = number => event => {
+    if (this.state.value.indexOf(".") == -1) {
+      this.setState({
+        value: this.state.value + event.target.value })
+    }
   }
 
-  handleSymbolChange = event => {
-  
+  handleSymbolChange = number => event => {
+    this.state.firstNumber = this.state.value
+    this.setState({value: "" })
+    this.state.symbol = event.target.value
   }
 
-  handleZeroChange = event => {
-   
+  handleZeroChange = number => event => {
+    if (this.state.value !== "") {
+      this.setState({ 
+        value: this.state.value + event.target.value})
+    }
+  }
+
+  handleEquation = number => event => {
+    if (this.state.value !== "") {
+      this.setState({ 
+        value: this.state.value + event.target.value})
+    }
   }
 
   submitEquation = () => {
-   
+    this.state.secondNumber = this.state.value
+    if (this.state.symbol == "+") {
+      this.setState({
+        value: Number(this.state.firstNumber) + Number(this.state.secondNumber) 
+      })
+    } else if (this.state.symbol == "-") {
+        this.setState({
+          value: Number(this.state.firstNumber) - Number(this.state.secondNumber) 
+        })
+    } else if (this.state.symbol == "/") {
+      this.setState({
+        value: Number(this.state.firstNumber) / Number(this.state.secondNumber) 
+      })
+    } else if (this.state.symbol == "*") {
+      this.setState({
+        value: Number(this.state.firstNumber) * Number(this.state.secondNumber) 
+      })
+    }
+    console.log('CLICK EQUALS STATE', this.state);
   }
 
 
@@ -46,29 +83,29 @@ render() {
     <div className="App">
     <div>
       <div>
-        <div>{this.state.input}</div>
+        <div>{this.state.value}</div>
       </div>
      <div>
-      <button onClick={this.handleChange('input')} value="2">7</button>
-      <button onClick={this.handleChange('input')} value="2">8</button>
-      <button onClick={this.handleChange('input')} value="2">9</button>
+      <button onClick={this.handleChange('value')} value="2">7</button>
+      <button onClick={this.handleChange('value')} value="2">8</button>
+      <button onClick={this.handleChange('value')} value="2">9</button>
       <button onClick={this.handleSymbolChange('symbol')} value="/">/</button>
      </div>
      <div>
-      <button onClick={this.handleChange('input')} value="4">4</button>
-      <button onClick={this.handleChange('input')} value="5">5</button>
-      <button onClick={this.handleChange('input')} value="6">6</button>
+      <button onClick={this.handleChange('value')} value="4">4</button>
+      <button onClick={this.handleChange('value')} value="5">5</button>
+      <button onClick={this.handleChange('value')} value="6">6</button>
       <button onClick={this.handleSymbolChange('symbol')} value="*">*</button>
      </div>
      <div>
-      <button onClick={this.handleChange('input')} value="1">1</button>
-      <button onClick={this.handleChange('input')} value="2">2</button>
-      <button onClick={this.handleChange('input')} value="3">3</button>
+      <button onClick={this.handleChange('value')} value="1">1</button>
+      <button onClick={this.handleChange('value')} value="2">2</button>
+      <button onClick={this.handleChange('value')} value="3">3</button>
       <button onClick={this.handleSymbolChange('symbol')} value="+">+</button>
      </div>
      <div>
-      <button nClick={this.handleDecimalChange('input')} value="3">.</button>
-      <button onClick={this.handleZeroChange('input')} value="0">0</button>
+      <button nClick={this.handleDecimalChange('value')} value=".">.</button>
+      <button onClick={this.handleZeroChange('value')} value="0">0</button>
       <button onClick={this.submitEquation}>=</button>
       <button onClick={this.handleSymbolChange('symbol')} value="-">-</button>
      </div>
